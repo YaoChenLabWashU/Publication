a6be96c5cf6ca069b5d51f2c1632d89f869b331f
pcmaaaaa <49624881+pcmaaaaa@users.noreply.github.com>
2025-05-25 14:44:57 -0700

    Delete Simulation_manuscript/SimulationAnalysis_inPython directory
    Remove the previous version of python code;
    will remove unneccessary data;
    will disable fitting and empirical lifetime calculation.

--- Simulation_manuscript/SimulationAnalysis_inPython/lib/misc.py
+++
@@ -1,6 +0,0 @@
-"""Misc"""
-import importlib.resources
-
-def get_resource_path(package, resource):
-  """Gets the file path of a resource within a package."""
-  return str(importlib.resources.files(package) / resource)
