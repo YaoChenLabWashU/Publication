523ddc025bc44553a6c4a5dbeb802e8f4d20c9ea
pcmaaaaa <49624881+pcmaaaaa@users.noreply.github.com>
2025-05-25 15:05:15 -0700

    Upload Python simulation code
    removed unnecessary data; disabled fitting and empirical lifetime calculation; cleaned up comments; added example command line for running simulation in readme file

---
+++ Simulation_manuscript/Simulation_inPython/lib/misc.py
@@ -0,0 +1,6 @@
+"""Misc functions"""
+import importlib.resources
+
+def get_resource_path(package, resource):
+  """Gets the file path of a resource within a package."""
+  return str(importlib.resources.files(package) / resource)
---
+++ Simulation_manuscript/Simulation_inPython/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='FLiSimBA',
+  version='0.1.0',
+  description='FLIM simulation',
+  packages=find_packages(),
+  python_requires='==3.9',
+)
