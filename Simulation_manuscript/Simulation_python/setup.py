ed28c2d716028e6347ab47b422508ce102338756
pcmaaaaa <49624881+pcmaaaaa@users.noreply.github.com>
2025-05-25 15:03:42 -0700

    Delete Simulation_manuscript/Simulation_python directory

--- Simulation_manuscript/Simulation_python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-  name='FLiSimBA',
-  version='0.1.0',
-  description='FLIM simulation',
-  packages=find_packages(),
-  python_requires='==3.9',
-)
